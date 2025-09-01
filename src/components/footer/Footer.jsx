8b05734edc1a3c764e7edc15dc5109814f402569
harneet <harneetpals@gmail.com>
2025-09-01 20:30:53 +0530

    cryto web page

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+function Footer(){
+    return(
+        <>
+        <div className='footer'>
+            <p>
+                Copyright @ 2025, Cryptobae -All Right Reserved.
+            </p>
+        </div>
+        </>
+    )
+}
+export default Footer;
